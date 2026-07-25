4ad9acc882f7b74378ab1d79de1da26a18563719
thegreenprintt <waltonjacob300@gmail.com>
2026-07-25 11:20:58 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -222,7 +222,7 @@
        form still shows the confirmation, but sends nothing.
        Get one free at https://formspree.io -> New Form.
        ===================================================== */
-    var FORM_ENDPOINT = ''; // e.g. 'https://formspree.io/f/xvzevwvw'
+    var FORM_ENDPOINT = 'https://formspree.io/f/xvzevwvw';
 
     var submitBtn = form.querySelector('button[type="submit"]');
 
